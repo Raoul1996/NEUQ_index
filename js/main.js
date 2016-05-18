e1713afaf8e70483d676adc5a5d15d081d8f0eeb
Raoul1996 <baobaozhou199606@gmail.com>
2016-05-18 17:22:22 +0800

    start

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/*
+* 块状注释区：
+* 我不喜欢原网页www.neuq.edu.cn的banner
+* 所以换了>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+* */
+    $(document).ready(function(){
+        $(document).ready(function() {
+            $(".box_skitter_large").skitter({
+                animation: "random",
+                interval: 3000,
+                numbers: false,
+                numbers_align: "right",
+                hideTools: true,
+                controls: false,
+                focus: false,
+                focus_position: true,
+                width_label:'340px',
+                enable_navigation_keys: true,
+                progressbar: false
+            });
+        });
+    });
